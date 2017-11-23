e289dfeb3ec252852f4854819c1e26a753f436a8
a <a@a-PC>
2017-11-23 22:02:17 +0900

    changed something

--- src/gyeol/calendar/Calendar.java
+++ src/gyeol/calendar/Calendar.java
@@ -4,12 +4,12 @@ import java.util.Scanner;
 
 public class Calendar {
 
-	private static final int MAX_DAYS[] = {31,28,31,30,31,30,31,31,30,31,30,31};
-	
+	private static final int MAX_DAYS[] = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+
 	public static int maxDaysOfMonth(int month) {
 		return MAX_DAYS[month - 1];
 	}
-	
+
 	static void printSample() {
 		System.out.println("일  월  화  수  목  금  토");
 		System.out.println("--------------------");
@@ -18,19 +18,18 @@ public class Calendar {
 		System.out.println("15 16 17 18 19 20 21");
 		System.out.println("22 23 24 25 26 27 28");
 	}
-	
+
 	public static void main(String[] args) {
-		
-		//숫자를 입력받아 해당하는 달의 일수를 출력하는 프로그램
-		
+
+		// 숫자를 입력받아 해당하는 달의 일수를 출력하는 프로그램
+
 		Scanner s = new Scanner(System.in);
-		
+
 		int month = s.nextInt();
-		
-		
+
 		try {
-		System.out.printf("%d월은 %d일입니다.\n", month, maxDaysOfMonth(month));
-		} catch(IndexOutOfBoundsException e) {
+			System.out.printf("%d월은 %d일입니다.\n", month, maxDaysOfMonth(month));
+		} catch (IndexOutOfBoundsException e) {
 			System.out.println("1월부터 12월까지밖에 없습니다.");
 		}
 		printSample();
